ea8ba5248034474b5edadc359651e6361eb5c5d0
Bhawna Chopra <chopra.bhawna1212@gmail.com>
2022-08-18 09:57:30 +0530

    Table with sum functionality created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
+ 
+import { useState, useEffect } from 'react';
+ 
+const Table = () => {
+  const [rows, setRows] = useState([[0, 0]]);
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <table >
+      <thead>
+        <tr>
+          <th>First</th>
+          <th>Second</th>
+          <th>Sum</th>
+        </tr>
+      </thead>
+      <tbody>
+        {rows.map((row, i) => {
+          return (
+            <Row rows= {rows} row={row} setRows={setRows} i = {i}/>
+          )
+        })}
+        <tr>
+          <td>{rows.map((row) => row[0]).reduce((a, b) => a + b, 0)}</td>
+          <td>{rows.map((row) => row[1]).reduce((a, b) => a + b, 0)}</td>
+          <td>
+            {rows.map((row) => row[0] + row[1]).reduce((a, b) => a + b, 0)}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <button
+      onClick={() => {
+        setRows([...rows, [0, 0]]);
+      }}
+    >
+    Add rows
+    </button>
+    </>
+  );
+};
+ 
+const Row = ({
+  rows,
+  row,
+  setRows,
+  i,
+}) => {
+  const [first, setFirst] = useState(0);
+  const [second, setSecond] = useState(0);
+  
+  useEffect(()=>{
+    const newrow = [...rows];
+    newrow[i][0] = first;
+    newrow[i][1] = second;
+    setRows(newrow);
+    console.log(rows[i]);
+    },[first, second])
+ 
+  return (
+    <tr>
+      <td>
+        <input
+          type="number"
+          onChange={(e) => {
+            setFirst(Number(e.target.value));
+          }}
+        />{' '}
+      </td>
+      <td>
+         <input
+          type="number"
+          onChange={(e) => {
+            setSecond(Number(e.target.value));
+          }}
+        />
+      </td>
+      <td>{first + second}</td>
+    </tr>
+  );
+};
+ 
+export default function App() {
+  return (
+    <main>
+      <Table />
+    </main>
   );
 }
-
-export default App;
